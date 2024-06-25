1025c41e009eaeb771a7787e81c9b5197c5deff8
Igor Vilkov <igorvilkov404@gmail.com>
2024-06-25 15:49:53 +0300

    initial commit

---
+++ src/test/java/io/igorv404/csv_parser/CsvParserApplicationTests.java
@@ -0,0 +1,11 @@
+package io.igorv404.csv_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvParserApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
