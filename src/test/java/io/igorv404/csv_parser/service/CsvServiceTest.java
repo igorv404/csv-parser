146d69e50bb0ee765562608966182ec332ddcc04
Igor Vilkov <71030441+igorv404@users.noreply.github.com>
2024-06-26 23:47:03 +0300

    tests (#10)

---
+++ src/test/java/io/igorv404/csv_parser/service/CsvServiceTest.java
@@ -0,0 +1,105 @@
+package io.igorv404.csv_parser.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.igorv404.csv_parser.exception.EmptyCsvException;
+import io.igorv404.csv_parser.exception.FileIsNotCsvException;
+import io.igorv404.csv_parser.model.Movie;
+import io.igorv404.csv_parser.repository.MovieRepository;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+class CsvServiceTest {
+
+  @Mock
+  private MovieRepository movieRepository;
+
+  @Mock
+  private MovieService movieService;
+
+  @InjectMocks
+  private CsvService csvService;
+
+  private final Movie entity1 = new Movie(1, " The Shawshank Redemption",
+      "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
+      82, 1, 1994, "2h 22m", "15", 9.3f, "(2.9M)");
+
+  private final Movie entity2 = new Movie(2, " The Godfather",
+      "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+      100, 2, 1972, "2h 55m", "15",
+      9.2f, "(2M)");
+
+  private final Movie entity3 = new Movie(3, " The Dark Knight",
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+      84, 3, 2008, "2h 32m", "12A",
+      9f, "(2.9M)");
+
+  private final List<Movie> data = List.of(this.entity1, this.entity2, this.entity3);
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void shouldUploadCsvData() {
+    try {
+      when(this.movieRepository.findAll()).thenReturn(this.data);
+      when(this.movieService.getAllEntities()).thenReturn(this.data);
+      when(this.movieRepository.saveAll(this.data)).thenReturn(this.data);
+      File csv = new File("src/test/resources/data.csv");
+      FileInputStream inputStream = new FileInputStream(csv);
+      MultipartFile multipartFile = new MockMultipartFile("file", "data.csv", "text/csv",
+          inputStream);
+      List<Movie> savedData = this.csvService.uploadCsvData(multipartFile);
+      assertNotEquals(0, savedData.size());
+      assertEquals(3, savedData.size());
+      assertEquals(this.data, savedData);
+      verify(this.movieService, times(1)).getAllEntities();
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void shouldThrowEmptyCsvExceptionWhenCsvFileIsEmpty() {
+    try {
+      File csv = new File("src/test/resources/empty.csv");
+      FileInputStream inputStream = new FileInputStream(csv);
+      MultipartFile multipartFile = new MockMultipartFile("file", "empty.csv", "text/csv",
+          inputStream);
+      assertThrows(EmptyCsvException.class, () -> this.csvService.uploadCsvData(multipartFile));
+      verify(this.movieRepository, times(0)).findAll();
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void shouldThrowFileIsNotCsvExceptionWhenFileIsNotCsv() {
+    try {
+      File csv = new File("src/test/resources/badExtension.txt");
+      FileInputStream inputStream = new FileInputStream(csv);
+      MultipartFile multipartFile = new MockMultipartFile("file", "badExtension.txt", "text/plain",
+          inputStream);
+      assertThrows(FileIsNotCsvException.class, () -> this.csvService.uploadCsvData(multipartFile));
+      verify(this.movieRepository, times(0)).findAll();
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+}
---
+++ src/test/java/io/igorv404/csv_parser/service/MovieServiceTest.java
@@ -0,0 +1,123 @@
+package io.igorv404.csv_parser.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.igorv404.csv_parser.model.Movie;
+import io.igorv404.csv_parser.repository.MovieRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class MovieServiceTest {
+  @Mock
+  private MovieRepository movieRepository;
+
+  @InjectMocks
+  private MovieService movieService;
+
+  private final Movie entity1 = new Movie(1, "Movie 1", "Description", 80, 4, 2004, "2h 22m", "15", 8.7f, "(2.9M)");
+
+  private final Movie entity2 = new Movie(2, "Movie 2", "Description", 87, 1, 2012, "1h 50m", "12", 9.1f, "(10.3M)");
+
+  private final List<Movie> data = List.of(this.entity1, this.entity2);
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void shouldReturnAllMovies() {
+    when(this.movieRepository.findAll()).thenReturn(data);
+    List<Movie> responseData = this.movieService.getAllEntities();
+    assertNotNull(responseData);
+    assertEquals(data.size(), responseData.size());
+    assertTrue(responseData.containsAll(data));
+    verify(this.movieRepository, times(1)).findAll();
+  }
+
+  @Test
+  public void shouldReturnMovieWithId2() {
+    final Integer id = 2;
+    when(this.movieRepository.findById(id)).thenReturn(Optional.of(this.entity2));
+    Movie responseData = this.movieService.getEntity(id);
+    assertNotNull(responseData);
+    assertEquals(this.entity2, responseData);
+    verify(this.movieRepository, times(1)).findById(id);
+  }
+
+  @Test
+  public void shouldThrowEntityNotFoundExceptionWhenCallMovieWithId0() {
+    final Integer id = 0;
+    when(this.movieRepository.findById(id)).thenReturn(Optional.empty());
+    assertThrows(EntityNotFoundException.class, () -> this.movieService.getEntity(id));
+    verify(this.movieRepository, times(1)).findById(id);
+  }
+
+  @Test
+  public void shouldCreateNewMovie() {
+    final Movie entity = new Movie(3, "Movie 3", "Description", 69, 15, 2000, "2h 24m", "18", 6.9f, "(900K)");
+    when(this.movieRepository.save(entity)).thenReturn(entity);
+    Movie responseData = this.movieService.createEntity(entity);
+    assertNotNull(responseData);
+    assertEquals(entity, responseData);
+    verify(this.movieRepository, times(1)).save(entity);
+  }
+
+  @Test
+  public void shouldUpdateMovie() {
+    final Integer id = 1;
+    final Movie updatedEntity = new Movie(1, "New movie 1", "Description", 80, 4, 2004, "2h 50m", "13", 8.9f, "(2.9M)");
+    when(this.movieRepository.findById(id)).thenReturn(Optional.of(this.entity1));
+    when(this.movieRepository.save(any(Movie.class))).thenReturn(updatedEntity);
+    Movie responseData = this.movieService.updateEntity(updatedEntity);
+    assertNotNull(responseData);
+    assertEquals(updatedEntity, responseData);
+    assertNotEquals(this.entity1, responseData);
+    verify(this.movieRepository, times(1)).findById(id);
+    verify(this.movieRepository, times(1)).save(any(Movie.class));
+  }
+
+  @Test
+  public void shouldThrowEntityNotFoundExceptionWhenTryToUpdateMovieWithAbsentId() {
+    final Integer id = 0;
+    final Movie badEntity = new Movie(0, "Bad movie", "Description", 0, 1000, 1999, "2h 50m", "16", 0.0f, "(2M)");
+    when(this.movieRepository.findById(id)).thenReturn(Optional.empty());
+    assertThrows(EntityNotFoundException.class, () -> this.movieService.updateEntity(badEntity));
+    verify(this.movieRepository, times(1)).findById(id);
+    verify(this.movieRepository, times(0)).save(badEntity);
+  }
+
+  @Test
+  public void shouldDeleteMovie() {
+    final Integer id = 1;
+    when(this.movieRepository.findById(id)).thenReturn(Optional.of(this.entity1));
+    String responseMessage = this.movieService.deleteEntity(id);
+    assertNotNull(responseMessage);
+    assertEquals(String.format("Movie \"%s\" was deleted", entity1.getName()), responseMessage);
+    verify(this.movieRepository, times(1)).findById(id);
+    verify(this.movieRepository, times(1)).deleteById(id);
+  }
+
+  @Test
+  public void shouldThrowEntityNotFoundExceptionWhenTryToDeleteMovieWithAbsentId() {
+    final Integer id = 0;
+    when(this.movieRepository.findById(id)).thenReturn(Optional.empty());
+    assertThrows(EntityNotFoundException.class, () -> this.movieService.deleteEntity(id));
+    verify(this.movieRepository, times(1)).findById(id);
+    verify(this.movieRepository, times(0)).deleteById(id);
+  }
+}
